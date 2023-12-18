b12fdbaecba0d8787914af55fc0890c963b63d2a
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-12-18 23:19:23 +0100

    bagfix controller update

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -1,6 +1,8 @@
 import express from "express";
 import contactsController from "../../controllers/contacts-controller.js";
 
+import { isEmplyBody } from "../../middlewares/index.js";
+
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", contactsController.getAllContacts);
@@ -11,6 +13,6 @@ contactsRouter.post("/", contactsController.addContact);
 
 contactsRouter.delete("/:contactId", contactsController.removeContact);
 
-contactsRouter.put("/:contactId", contactsController.updateContact);
+contactsRouter.put("/:contactId", isEmplyBody, contactsController.updateContact);
 
 export default contactsRouter;
--- schemas/schemas-contact.js
+++ schemas/schemas-contact.js
@@ -2,31 +2,49 @@ import Joi from "joi";
 
 export const contactAddSchema = Joi.object({
   name: Joi.string()
-    .trim()
     .min(2)
     .pattern(/^[a-zA-Zа-яА-Я]+([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*$/)
     .required()
     .messages({
       "any.required": "missing required name field",
-      "any.pattern":
+      "string.pattern.base":
         "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan",
     }),
   email: Joi.string()
-    .trim()
-    .min(10)
-    .pattern(/^\+?\d{1,4}?[ .\-s]?(\(\d{1,3}?\))?([ .\-s]?\d{1,4}){1,4}$/)
+    .pattern(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
     .required()
     .messages({
       "any.required": "missing required email field",
-      "any.pattern": "Phone number must be digits and can contain spaces, dashes, parentheses and can start with +",
+      "string.pattern.base": "Invalid email address format",
     }),
-  phone: Joi.string().required().messages({
-    "any.required": "missing required phone field",
-  }),
+  phone: Joi.string()
+    .min(7)
+    .pattern(/^[\d ()-]+$/)
+    .required()
+    .messages({
+      "any.required": "missing required phone field",
+      "string.pattern.base": "Phone number must be digits and can contain spaces, dashes, parentheses",
+    }),
 });
 
 export const contactUpdateSchema = Joi.object({
-  name: Joi.string(),
-  email: Joi.string(),
-  phone: Joi.string(),
+  name: Joi.string()
+    .min(2)
+    .pattern(/^[a-zA-Zа-яА-Я]+([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*$/)
+    .messages({
+      "string.pattern.base":
+        "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan",
+    }),
+  email: Joi.string()
+    .min(4)
+    .pattern(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
+    .messages({
+      "string.pattern.base": "Invalid email address format",
+    }),
+  phone: Joi.string()
+    .min(7)
+    .pattern(/^[\d ()-]+$/)
+    .messages({
+      "string.pattern.base": "Phone number must be digits and can contain spaces, dashes, parentheses",
+    }),
 });
