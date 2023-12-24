71f8d1c095ecaf05c4d17b021703b5af361be33d
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-12-24 23:21:30 +0100

    final all

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -8,48 +8,52 @@ const getAllContacts = async (req, res) => {
   res.json(result);
 };
 
-// const getById = async (req, res, next) => {
-//     const { contactId } = req.params;
-//     const result = await contactsService.getContactById(contactId);
-//     if (!result) {
-//       throw HttpError(404);
-//     }
-//     res.json(result);
-// };
+const getById = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await Contact.findById(contactId);
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json(result);
+};
 
-// const removeContact = async (req, res, next) => {
-
-//     const { contactId } = req.params;
-//     const result = await contactsService.removeContact(contactId);
-//     if (!result) {
-//       throw HttpError(404);
-//     }
-//     res.json({ message: "contact deleted" });
-// };
+const removeContact = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndDelete(contactId);
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json({ message: "contact deleted" });
+};
 
 const addContact = async (req, res, next) => {
   const result = await Contact.create(req.body);
   res.status(201).json(result);
 };
 
-// const updateContact = async (req, res, next) => {
+const updateContact = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body);
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json(result);
+};
 
-//     const { error } = contactUpdateSchema.validate(req.body);
-//     if (error) {
-//       throw HttpError(400, error.message);
-//     }
-//     const { contactId } = req.params;
-//     const result = await contactsService.updateContact(contactId, req.body);
-//     if (!result) {
-//       throw HttpError(404);
-//     }
-//     res.json(result);
-// };
+const updateStatusContact = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body);
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json(result);
+};
 
 export default {
   getAllContacts: ctrlWrapper(getAllContacts),
-  // getById: ctrlWrapper(getById),
-  // removeContact: ctrlWrapper(removeContact),
+  getById: ctrlWrapper(getById),
+  removeContact: ctrlWrapper(removeContact),
   addContact: ctrlWrapper(addContact),
-  // updateContact: ctrlWrapper(updateContact),
+  updateContact: ctrlWrapper(updateContact),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
 };
--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -1,20 +1,32 @@
 import express from "express";
 import contactsController from "../../controllers/contacts-controller.js";
-import { contactAddSchema, contactUpdateSchema } from "../../models/Contacts.js";
+import { contactAddSchema, contactUpdateFavoriteSchema, contactUpdateSchema } from "../../models/Contacts.js";
 
-import { isEmplyBody } from "../../middlewares/index.js";
+import { isEmplyBody, isValidId } from "../../middlewares/index.js";
 import validateBody from "../../decorators/validateBody.js";
 
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", contactsController.getAllContacts);
 
-// contactsRouter.get("/:contactId", contactsController.getById);
+contactsRouter.get("/:contactId", isValidId, contactsController.getById);
 
-contactsRouter.post("/", isEmplyBody, validateBody(contactAddSchema), contactsController.addContact);
+contactsRouter.post("/", isEmplyBody("missing fields"), validateBody(contactAddSchema), contactsController.addContact);
 
-// contactsRouter.delete("/:contactId", validateBody(contactUpdateSchema), contactsController.removeContact);
+contactsRouter.delete("/:contactId", isValidId, contactsController.removeContact);
 
-// contactsRouter.put("/:contactId", isEmplyBody, contactsController.updateContact);
-
+contactsRouter.put(
+  "/:contactId",
+  isValidId,
+  isEmplyBody("missing fields"),
+  validateBody(contactUpdateSchema),
+  contactsController.updateContact
+);
+contactsRouter.patch(
+  "/:contactId/favorite",
+  isValidId,
+  isEmplyBody("missing field favorite"),
+  validateBody(contactUpdateFavoriteSchema),
+  contactsController.updateStatusContact
+);
 export default contactsRouter;
